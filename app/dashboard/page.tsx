c686920e9cb392270e880d62115937eace684057
ahmad zaeni mubarok <ahmadzaenimubarok121@gmail.com>
2025-04-24 10:52:16 +0700

    learning next js

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1 className="text-red-500 text-7xl">Hello wordl</h1>
+    );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1 className="text-red-500 text-7xl">Tidak Ditemukan</h1>
+    );
+}
